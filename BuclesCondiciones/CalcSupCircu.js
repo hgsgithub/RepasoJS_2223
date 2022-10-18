201717c3f1063c1bd3f9f62e57e99ee912276d20
hgsgithub <62034695+hgsgithub@users.noreply.github.com>
2022-10-18 20:46:35 +0200

    18_10_V2

---
+++ BuclesCondiciones/CalcSupCircu.js
@@ -0,0 +1,29 @@
+			/* Visualización del nombre del script */
+			alert("Calculo Superficie Circulos");
+
+			/* Declaración de variables locales */
+			var radio1, radio2, pi, superficie1, superficie2, diferencia;
+
+			/* Inicialización de las variables */
+			//radio1 = 5.5;
+			//radio2 = 3.5;
+			pi = 3.14;
+			//Tambien se puede introducir por teclado, cuidado string
+			radio1=parseInt(prompt("radio1: "))
+			console.log(radio1)
+			console.log(typeof radio1)
+			radio2=parseInt(prompt("radio2: "));
+			console.log(typeof radio2)
+			console.log(radio2)
+			console.log(radio1+radio2)
+
+			/* Cálculos */
+			superficie1 = pi * radio1 * radio1;
+			superficie2 = pi * radio2 * radio2;
+			diferencia = superficie1 - superficie2;
+
+			/* Visualización de los resultados */
+			document.write("Superficie 1 : " + superficie1 + "<br />");
+			document.write("Superficie 2 : " + superficie2 + "<br />");
+			alert("Diferencia : " + diferencia);
+
---
+++ BuclesCondiciones/Condicion2.js
@@ -0,0 +1,8 @@
+edad=prompt("Edad : ");
+ if(edad>=18 && edad<=65){
+    document.write("Apto para el trabajo, eres Adulto")
+    console.log("Apto para el trabajo, eres Adulto");
+}else {
+    document.write("No eres Apto para el trabajo")
+    console.log("No eres Apto para el trabajo");
+}  
